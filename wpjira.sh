7d10a965fbd882b64626d92ebe32672c2ef7e2f9
Jerry Gamblin <jerry.gamblin@gmail.com>
2017-11-14 15:23:39 -0600

    First Commit
    First Commit

---
+++ wpjira.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+url="$1"
+jiraurl=""
+jirausername=""
+jirapassword=""
+newestwp=$(curl -sSL http://api.wordpress.org/core/version-check/1.7 | jq -r .offers[0].current)
+runningwp=$(curl -s -N $url | grep 'wp-embed.min.js?ver=' | sed 's:.*=::' | cut -c1-5)
+
+if [ "$newestwp" == "$runningwp" ] ; then
+  printf "You are running Wordpress %s. Which is the newst Version.\n" "$newestwp"
+  sleep 3600
+else
+  curl -D- -u $jirausername:$jirapassword -X POST --data '{"fields":{"project":{"key": "CFXSEC"},"summary": "Wordpress is Out Of Date","description": "'$URL' is running '$runningwp' and '$newestwp' is the latest version.","issuetype": {"name": "Story"}}}' -H "Content-Type: application/json" $jiraurl
+  sleep 604800
+fi
---
+++ wpversion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+url="$1"
+newestwp=$(curl -sSL http://api.wordpress.org/core/version-check/1.7 | jq -r .offers[0].current)
+runningwp=$(curl -s -N $url | grep 'wp-embed.min.js?ver=' | sed 's:.*=::' | cut -c1-5)
+
+if [ "$newestwp" == "$runningwp" ] ; then
+  printf "%s is running Wordpress %s. Which is the newst Version.\n" "$url" "$newestwp"
+else
+  printf "%s is out of date. You are running %s and %s is the latest version.\n" "$url" "$runningwp" "$newestwp"
+fi
