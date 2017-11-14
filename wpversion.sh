c5e513d9c36da0e0590fc16ab007f9130c015ab8
Jerry Gamblin <jerry.gamblin@gmail.com>
2017-11-14 15:37:09 -0600

    Update wpversion.sh

--- wpversion.sh
+++ wpversion.sh
@@ -4,7 +4,7 @@ newestwp=$(curl -sSL http://api.wordpress.org/core/version-check/1.7 | jq -r .of
 runningwp=$(curl -s -N $url | grep 'wp-embed.min.js?ver=' | sed 's:.*=::' | cut -c1-5)
 
 if [ "$newestwp" == "$runningwp" ] ; then
-  printf "%s is running Wordpress %s. Which is the newst Version.\n" "$url" "$newestwp"
+  printf "%s is running Wordpress %s. Which is the newst version.\n" "$url" "$newestwp"
 else
   printf "%s is out of date. You are running %s and %s is the latest version.\n" "$url" "$runningwp" "$newestwp"
 fi
